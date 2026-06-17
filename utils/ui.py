4c24613737c2002b12bec7204f933e5983df4311
stebil87 <97510620+stebil87@users.noreply.github.com>
2026-06-17 11:18:39 +0200

    Create ui.py

---
+++ utils/ui.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+LOGO_PATH = "assets/godenzi.png"
+
+
+def mostra_logo():
+    col_logo, col_titolo = st.columns([1, 4])
+    with col_logo:
+        st.image(LOGO_PATH, width=120)
+    with col_titolo:
+        st.markdown(
+            """
+            <div style="padding-top: 25px;">
+                <span style="font-size: 1.1em; font-weight: bold;">IVO GODENZI &nbsp; LATTONIERE</span><br>
+                <span>RIVA S. VITALE</span>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+    st.divider()
